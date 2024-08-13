ab6428709329b630d0a08483f27ac22488ebd721
Diogo Oliveira Carvalho <diogo.oliveira0012@gmail.com>
2024-08-13 15:01:23 -0300

    Correção bipartido e Adição de struct

--- executar.cpp
+++ executar.cpp
@@ -7,13 +7,20 @@
 using namespace std;
 
 // Cores que representam o estado dos vértices durante uma busca
-#define BRANCO 0    // Vértice não descoberto
+#define BRANCO 0     // Vértice não descoberto
 #define CINZA 1      // Vértice descoberto
 #define PRETO 2      // Vértice fechado
 
 // Cores utilizadas em problemas de coloração de grafos
 #define AZUL 3
 #define VERMELHO 4  
+
+// Representa uma aresta em uma lista de adjacência
+struct aresta{
+    int id;             // Código identificador única da aresta
+    int vertice;        // Vértice a quem a aresta se liga 
+    int peso;           // Peso da aresta
+};
 
 // Tipo do grafo 
 enum TipoGrafo{
@@ -22,8 +29,9 @@ enum TipoGrafo{
 };
 
 // Lê os dados do grafo passados pelo usuário e retorna sua lista de adjacência
-vector<pair<int, int>>* lerGrafo(int n, int m, enum TipoGrafo tipo, vector<pair<int, int>>* LA) {
-    int id, u, v, w;
+vector<aresta>* lerGrafo(int n, int m, enum TipoGrafo tipo, vector<aresta>* LA) {
+    int id, u, v, w;            // Identificador da aresta, dois vértices conectados por ela e seu peso
+    int* auxId = new int[n];    // Vetor auxiliar para verificar id's repetidos
 
     for(int i = 0; i < m; i++) {
         cin >> id >> u >> v >> w;
@@ -33,17 +41,26 @@ vector<pair<int, int>>* lerGrafo(int n, int m, enum TipoGrafo tipo, vector<pair<
             continue;
         }
 
+        // Evitando a leitura de arestas com id já existente
+        for(int i = 0; i < n; i++) {
+            for(auto i: LA[u]) {
+                if(i.id == id) {
+                    continue;
+                }
+            }
+        }
+
         // Evitando a leitura de arestas repetidas
         for(auto i: LA[u]) {
-            if(i.first == v) {
+            if(i.vertice == v) {
                 continue;
             }
         }
 
         // Adicionando as arestas à lista de adjacência
-        LA[u].push_back({v,w});
+        LA[u].push_back({id, v, w});
         if(tipo == nao_direcionado) {
-            LA[v].push_back({u,w});
+            LA[v].push_back({id, u, w});
         }  
     }
 
@@ -51,7 +68,7 @@ vector<pair<int, int>>* lerGrafo(int n, int m, enum TipoGrafo tipo, vector<pair<
 }
 
 // Busca em Largura utilizada para verificar a conexidade do grafo
-int* bfsConexidade(int n, vector<pair<int, int>>* LA) {              
+int* bfsConexidade(int n, vector<aresta>* LA) {              
     int* cor = new int[n];  // Vetor de cores dos vértices
     queue<int> fila;        // Fila de vértices visitados durante a exploração do grafo
     int s = 0;              // Vértice inicial da busca
@@ -71,34 +88,35 @@ int* bfsConexidade(int n, vector<pair<int, int>>* LA) {
 
         // Percorrendo a lista de adjacência de u e procurando por vértices brancos
         for(auto v: LA[u]) {
-            if(cor[v.first] == BRANCO) {
-                cor[v.first] = CINZA;   // Vértice v descoberto
-                fila.push(v.first);     // Vértice v adicionado à fila
+            if(cor[v.vertice] == BRANCO) {
+                cor[v.vertice] = CINZA;   // Vértice v descoberto
+                fila.push(v.vertice);     // Vértice v adicionado à fila
             }
         }
     }
+
     return cor;   
 }
 
 // Verifica a conexidade do grafo
 // Retorna 1 se for conexo (nao_direcionado) ou fracamente conexo (direcionado) ou 0 se for desconexo
-int conexidadeGrafo(int n, enum TipoGrafo tipo, vector<pair<int, int>>* LA) {
+int conexidadeGrafo(int n, enum TipoGrafo tipo, vector<aresta>* LA) {
     int* cor = NULL;    // Vetor de cores dos vértices
 
     // Se o grafo for direcionado, para cada aresta em (u,v) é adicionada uma aresta (v,u), caso não exista
     if(tipo == direcionado) {
-        vector<pair<int, int>>* LA_Aux = new vector<pair<int, int>>[n];   // Lista de adjacência auxiliar para verificar conexidade fraca
+        vector<aresta>* LA_Aux = new vector<aresta>[n];   // Lista de adjacência auxiliar para verificar conexidade fraca
         
         for(int u = 0; u < n; u++) {
             for(auto v: LA[u]) {
                 LA_Aux[u].push_back(v);   // Adicionando a aresta (u,v) na LA_Aux
-                // Verificando se já existe a aresta (v,u) em LA e, caso não exista, ela é inserida na LA_Aux 
-                for(auto i: LA[v.first]) {
-                    if(i.first == u) {
+                // Verificando se já existe a aresta (v,u) na LA
+                for(auto i: LA[v.vertice]) {
+                    if(i.vertice == u) {
                         continue;
                     }
                 }
-                LA_Aux[v.first].push_back({u, v.second});
+                LA_Aux[v.vertice].push_back({v.id, u, v.peso});     // Criando a aresta (v,u) na LA_Aux
             }
         }
         cor = bfsConexidade(n, LA_Aux);    // Passando a LA_Aux como parâmetro para a BFS em caso de grafos direcionados
@@ -121,10 +139,10 @@ int conexidadeGrafo(int n, enum TipoGrafo tipo, vector<pair<int, int>>* LA) {
 
 
 // Verifica se o vértice é bipartido e retorna 1 caso seja verdadeiro ou 0 caso contrário
-int bipartido(int n, vector<pair<int,int>>* LA) {
+int bipartido(int n, vector<aresta>* LA) {
     int* corBFS = new int[n];       // Vetor de cores dos vértices indicando seu estado durante a BFS
     int* coloracao = new int[n];    // Vetor de cores dos vértices indicando sua partição através de sua coloração   
-    int auxColoracao = AZUL;    // Variável auxiliar para colorir o grafo
+    int auxColoracao = AZUL;        // Variável auxiliar para colorir o grafo
     queue<int> fila;                // Fila de vértices explorados durante a BFS
     int s = 0;                      // Vértice inicial da busca
 
@@ -145,7 +163,7 @@ int bipartido(int n, vector<pair<int,int>>* LA) {
         fila.pop();             // Retirando o vértice u da fila
 
         // Alterando a cor de auxColoracao
-        if(auxColoracao == VERMELHO) {
+        if(coloracao[u] == VERMELHO) {
             auxColoracao = AZUL;
         } else {
             auxColoracao = VERMELHO;
@@ -153,13 +171,13 @@ int bipartido(int n, vector<pair<int,int>>* LA) {
 
         // Percorrendo a LA de u e procurando por vértices brancos
         for(auto v: LA[u]) {
-            if(corBFS[v.first] == BRANCO) {
-                corBFS[v.first] = CINZA;               // Vértice v descoberto
-                coloracao[v.first] = auxColoracao;     // Colorindo o vértice v 
-                fila.push(v.first);                     // Adicionando v à fila
+            if(corBFS[v.vertice] == BRANCO) {
+                corBFS[v.vertice] = CINZA;                  // Vértice v descoberto
+                coloracao[v.vertice] = auxColoracao;        // Colorindo o vértice v 
+                fila.push(v.vertice);                       // Adicionando v à fila
             } else {
                 // Verificando se v tem a mesma cor de u
-                if(coloracao[v.first] == coloracao[u]) {
+                if(coloracao[v.vertice] == coloracao[u]) {
                     return 0;
                 }
             }
@@ -196,7 +214,7 @@ int main() {
     }
 
     // Lista de adjacência do grafo
-    vector<pair<int, int>>* LA = new vector<pair<int, int>>[n]; 
+    vector<aresta>* LA = new vector<aresta>[n]; 
 
     // Lendo as arestas do grafo
     LA = lerGrafo(n, m, tipo, LA);
